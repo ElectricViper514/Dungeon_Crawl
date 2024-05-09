03b64c5eec1765cb320f1b7a1378a52119879ada
John W Davis <ElectricViper@VipersByteSolutions.com>
2024-05-09 12:02:23 -0500

    I have added additional information for the defautl username and password.

--- Dungeon_Crawl.py
+++ Dungeon_Crawl.py
@@ -15,6 +15,7 @@ __maintainer__  = "John W. Davis"
 __email__       = "ElectricViper@VipersByteSolutions.com"
 __status__      = "Prototype" #"Development" | "Production"
 
+from asyncio.windows_events import NULL
 import os
 import random
 
@@ -39,25 +40,32 @@ input("Press 'Enter' when you are ready to move on.")
 
 print("""\n
       As you look around the room you see that you are in a small rectangular room
-      make with rough stone walls and at one time it looks to have been a pantry. As you
+      made with rough stone walls and at one time it looks to have been a pantry. As you
       investigate the pantry, you realize that you are only wearing some very basic
       garments. No, make that more like dirty rags. What'ev, as you look around the room
       you find and old chef's knife. No sooner have you picked up the knife that you hear
       the squeeking of an ROUS or Rodents of Unusual Size. In laymen's terms ... a giant rat!
       
       Prepare to fight, because one of you two is on the menu for supper tonight.
+      And I don't think that you want it to be you that's on the dinner menu.
       """)
 
-input("Press 'Enter' when you are ready to move on.\n\n")
+input("Remember to just breathe. When you have collected your thoughs, press 'Enter' to continue.\n\n")
 
-character_name = input("What is your name, brave soul?\nIf you don't answer, I will pick one for you:   ")
-character_gender = input("What gender do you identify as?\nIf you don't pick one, I'll set you as 'neutral':   ")
+name = input("What is your name, brave soul?\nIf you don't answer, I will pick one for you:   ")
+if not name:
+    name = "Soandso"
+gender = input("What gender do you identify as?\nIf you don't pick one, I'll set you as 'neutral':   ")
+if not gender:
+    gender = "Neutral"
+race = "Human"
 
 class PlayerCharacter:
 
-    def __init__(self, name = "Soandso", gender = "neutral", strength = 10, dexterity = 10, constitution = 10, alive = True):
+    def __init__(self, name, gender, race, strength = 10, dexterity = 10, constitution = 10, alive = True):
         self.name = name 
         self.gender = gender
+        self.race = race
         self.strength = strength
         self.dexterity = dexterity
         self.constitution = constitution
@@ -67,6 +75,9 @@ class PlayerCharacter:
         self.weapon = "Rusty Kitchen Knife"
         self.armor_upper_body = "Rags"
         self.armor_lower_body = "Rags"
+
+    def __repr__(self):
+        return f'PlayerCharacter("All you know is that you are a {self.gender} {self.race} and you think that your name is "{self.name}"\nYou think that is your name at least.)'
     
     def attack(self, attack_rating, damage):
         self.attack_rating = attack_rating
@@ -85,33 +96,42 @@ class PlayerCharacter:
         pass
 
 
-hero_pc = PlayerCharacter(character_name, character_gender)
+hero_pc = PlayerCharacter(name, gender, race)
 
-
+# This section is just for testing to make sure that the fields are being properly set.
 print(hero_pc.name)
+print(hero_pc.race)
 print(hero_pc.gender)
 print(hero_pc.strength)
 print(hero_pc.dexterity)
 print(hero_pc.constitution)
 print(hero_pc.level)
 print(hero_pc.experience)
+# This is the end of the testing section for the entries for the PlayerCharacter class.
 
+#This is the dictionary of weapons, armor, potions and monsters.
+# "Weapon name": Damage value
 dict_weapons = {"Rusty Kitchen Knife": 5, "Shortsword": 10, "Longsword": 15, "Greatsword": 20}
-dict_armors = {"Rags": 1, "Cloth Armor": 5, "Leather Armor": 10, "Chainmail": 15, "Platemail": 20}
-# {"Monster Name": [hitpoints, attack_rating, damage, armor_rating, experience_value, alive]}
-dict_monsters = {"ROUS": [5, 4, 3, 4, 7, True]}
+# "Armor name": [Defence value, cost]
+dict_armors = {"Rags": [1, 0], "Cloth Armor": [5, 10], "Leather Armor": [10, 25], "Chainmail": [20, 70], "Platemail": [50, 200]}
+# "Healing potion name": [Healing value, cost].
+dict_potions = {"Potion of healing": [30, 15], "Elixir of healing": [60, 40], "Infustion of healing": [120, 100], "Tincture of healing": [300, 250]}
+# {"Monster Name": [hitpoints, attack_rating, damage, armor_rating, experience_value, gold multiplier, alive]}
+# The gold multiplier is to reduce or increase the value earned based on the creatures difficutly.
+dict_monsters = {"ROUS": [5, 4, 3, 4, 7, 0.7, True]}
 
 
 class Monster:
     # The goal here is to have a single monster class and it can be populated with various monsters
     # from the dictionary to provide unique monsters.
-    def __init__(self, name, hitpoints, attack_rating, damage, armor_rating, experience_value, alive):
+    def __init__(self, name, hitpoints, attack_rating, damage, armor_rating, experience_value, gold_multiplier, alive):
         self.name = name
         self.hitpoints = hitpoints
         self.attack_rating = attack_rating
         self.damage = damage
         self.armor_rating = armor_rating
         self.experience_value = experience_value
+        self.gold_multiplier = gold_multiplier
         self.alive = True
 
 
