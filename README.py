2224249075a9f644a216f51261a3beff8e2b9db4
ElectricViper514 <167271596+ElectricViper514@users.noreply.github.com>
2024-05-16 10:57:10 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,17 +1,18 @@
+"""
 This is my first portfolio project from Codecademy.com from their Computer Science course. I have completed their intro to programming using the Python language. Now I am attempting to put together everything that I have learned up to this point. I have been working on this bit by bit and slowly expanding each section until I am satisfied with the base functionality.
 
 This project is called "Dungeon Crawl". It is a simple text adventure game where you are thrust into a damp and dark cellar where you must find and fight your way out to freedom. But be warned. You can never go back the way that you came from in this adventure. Or you can try to go back and see how it works out for you. <you hear the sounds of maniacal laughter fading into the distance>
 
 
 My original notes are below:
-'''These are my basic thoughts and notes for my first portfolio project.
+These are my basic thoughts and notes for my first portfolio project.
 I plan on designing a simple RPG dungeon crawl.
 For simplicity sake, I will only have a single class and race. A human fighter.
 Other races and classes may be implemented in at a later time.
 The goal is to have a very basic combat system, item shop, and character advancement.
-This is will be a short game.'''
+This is will be a short game.
 
-'''The scope of the game is as follows:
+The scope of the game is as follows:
 The player will name their character, select gender, and select their starting stats based on
 a stat pool that they can use to distribute to their characteristics.
 The player can equip weapons and armor
@@ -37,7 +38,8 @@ You are greeted by the grounds keeper who offers you a meal.
 You awaken in dungeon with only one option. To fight your way out in an attempt to
 escape your prison.
 
-The only way out is forward, never and I never go back the way you came.'''
+The only way out is forward, never and I never go back the way you came.
+"""
 
 # Example of simplifying a class.
 # https://stackoverflow.com/questions/1639174/creating-class-instance-properties-from-a-dictionary
