5e592527ad69e81f38b022e8d61e53af3c849d60
John W. Davis <ElectricViper@VipersByteSolutions.com>
2024-07-09 16:39:28 -0500

    I have updated the room discriptions so now they display correctly. Combat seems to move smoothly. That only leaves checking on the level system and to setup Potion tracking and store purchase.

--- Scratch Paper.py
+++ Scratch Paper.py
@@ -1,11 +1,17 @@
-import math
-level = 1
-level_max = 30
-xp_to_level = 0
+import time
 
-print("The exp needed for level 1 is 0.")
-while level < (level_max + 1):
-    xp_to_level = (100 * level) ** 1.15
-    exp = round(xp_to_level)
-    print(f"Level {level}: XP needed to the next level is '{exp}'.")
-    level += 1 
+text_bold = '\033[1m'
+text_underline = '\033[4m'
+text_invert = '\33[7m'
+text_color_purple = '\033[95m'
+text_color_blue = '\033[94m'
+text_color_cyan = '\033[96m'
+text_color_green = '\033[92m'
+text_color_orange = '\033[93m'
+text_color_red = '\033[91m'
+text_end = '\033[0m'
+
+string = (f"{text_color_cyan}{text_bold}Hey user whatup? Where are you! I am trying to debug this issue since I installed the update.jas3kljowijt;asd awstUZIPjkasfmpipwe. asdat as .as  datw.t24t zgf x. aseddoiljmnasvosehseovl;asgo   hsreo h{text_end}")
+for char in string:
+    print(char, end='')
+    time.sleep(.07)
